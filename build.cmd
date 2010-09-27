98989aecba5cb5892a37253cd0e15c3f54f892f5
Remo Gloor <remo.gloor@bbv.ch>
2010-09-27 08:44:47 +0200

    Fixed assembly information in build scripts

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-tools\nant\nant.exe -buildfile:Ninject.Extensions.ChildKernel.build %1 %2 %3 %4 %5 %6 %7 %8
+tools\nant\nant.exe -buildfile:Ninject.Extensions.Conventions.build %1 %2 %3 %4 %5 %6 %7 %8
 
 pause
