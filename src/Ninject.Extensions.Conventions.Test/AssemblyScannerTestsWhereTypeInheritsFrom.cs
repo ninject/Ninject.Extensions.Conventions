34296b2e0d8649d15ed96b42eba3db3f6e0211de
Ian Davis <ian@innovatian.com>
2011-02-13 15:52:12 -0800

    Adding more testing for issue #5.

--- src/Ninject.Extensions.Conventions.Test/AssemblyScannerTestsWhereTypeInheritsFrom.cs
+++ src/Ninject.Extensions.Conventions.Test/AssemblyScannerTestsWhereTypeInheritsFrom.cs
@@ -1,40 +1,58 @@
 ﻿namespace Ninject.Extensions.Conventions
 {
-    using System.Linq;
-    using Ninject.Extensions.Conventions.Fakes;
-    using Ninject.Infrastructure;
 #if SILVERLIGHT
-#if SILVERLIGHT_MSTEST
-    using MsTest.Should;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Fact = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
 #else
-    using UnitDriven;
-    using UnitDriven.Should;
-    using Fact = UnitDriven.TestMethodAttribute;
-#endif
-#else
-    using Ninject.Tests.MSTestAttributes;
+    using System.Reflection;
+    using System.Linq;
+    using Fakes;
+    using Tests.MSTestAttributes;
     using Xunit;
     using Xunit.Should;
-#endif
 
     [TestClass]
     public class AssemblyScannerTestsWhereTypeInheritsFrom
     {
+        private static readonly MethodInfo ProcessMethod =
+            typeof (AssemblyScanner).GetMethod("Process",
+                                               BindingFlags.Instance | BindingFlags.NonPublic |
+                                               BindingFlags.DeclaredOnly,
+                                               null,
+                                               new [] {typeof (IKernel)}, null);
+
         [Fact]
         public void DefaultConventionsApply()
         {
             using (IKernel kernel = new StandardKernel())
             {
-                AssemblyScanner scanner = new AssemblyScanner();
+                var scanner = new AssemblyScanner();
                 scanner.From(typeof(DefaultView).Assembly);
                 scanner.WhereTypeInheritsFrom<IDefaultView>();
                 scanner.BindWithDefaultConventions();
-                scanner.Process(kernel);
+                ProcessMethod.Invoke(scanner, new[]{kernel});
                 kernel.GetBindings(typeof(IDefaultView)).Count().ShouldBe(1);
                 kernel.Get<IDefaultView>().ShouldBeInstanceOf<DefaultView>();
+                scanner.BindingGenerators.Count.ShouldBe(1);
+                scanner.BindingGenerators[0].ShouldBeInstanceOf<DefaultBindingGenerator>();
+            }
+        }
+
+        [Fact]
+        public void RegexBindingGeneratorCanBeUsed()
+        {
+            var regexBindingGenerator = new RegexBindingGenerator("(I)(?<name>.+)(View)");
+            using (IKernel kernel = new StandardKernel())
+            {
+                var scanner = new AssemblyScanner();
+                scanner.From(typeof(DefaultView).Assembly);
+                scanner.WhereTypeInheritsFrom<IDefaultView>();
+                scanner.BindWith(regexBindingGenerator);
+                ProcessMethod.Invoke(scanner, new[]{kernel});
+                kernel.GetBindings(typeof(IDefaultView)).Count().ShouldBe(1);
+                kernel.Get<IDefaultView>().ShouldBeInstanceOf<DefaultView>();
+                scanner.BindingGenerators.Count.ShouldBe(1);
+                scanner.BindingGenerators[0].ShouldBeInstanceOf<RegexBindingGenerator>();
             }
         }
     }
+#endif
 }
